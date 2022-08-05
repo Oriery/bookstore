6efa6625ea4febaac5e2695c0065822266437d0e
Oriery <83728532+Oriery@users.noreply.github.com>
2022-08-05 08:59:30 +0000

    fixes #5

--- srv/external/API_BUSINESS_PARTNER.js
+++ srv/external/API_BUSINESS_PARTNER.js
@@ -1,12 +1,10 @@
 module.exports = srv => {
     const { A_BusinessPartner } = srv.entities
 
-    srv.on(['CREATE', 'UPDATE', 'DELETE'], A_BusinessPartner, req => {
-        const payload = { KEY: [{BUSINESSPARTNER: req.data.BusinessPartner}]}
-
-        cds.run(req.query)
+    srv.after(['CREATE', 'UPDATE', 'DELETE'], A_BusinessPartner, data => {
+        const payload = { KEY: [{BUSINESSPARTNER: data.BusinessPartner}]}
 
         srv.emit('BusinessPartner/Changed', payload)
-        console.log('Event emitted "BusinessPartner/Changed": ', payload, req.event)
+        console.log('Event emitted "BusinessPartner/Changed": ', payload)
     })
 }
